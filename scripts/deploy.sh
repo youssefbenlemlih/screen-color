96a63aa34aad20d4d7ed4b2ec9b76cd45d7b0a81
Youssef <youssefbenlemlih7@gmail.com>
2023-02-17 14:07:37 +0100

    use gh-pages action

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,16 +2,9 @@
 
 # abort on errors
 set -e
-
 # build
 npm install
 npm run build
 
 # place .nojekyll to bypass Jekyll processing
 echo > dist/.nojekyll
-
-#git config --global user.email "ci@example.com"
-#git config --global user.name "CI"
-
-#git push -f git@github.com:youssefbenlemlih/screen-color.git main:gh-pages
-gh-pages -d dist
