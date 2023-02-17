86e3ddd3a213f16dc19aa36570acf04d5ad1e111
Youssef <youssefbenlemlih7@gmail.com>
2023-02-17 14:01:52 +0100

    use gh-pages package

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,20 +7,11 @@ set -e
 npm install
 npm run build
 
-# navigate into the build output directory
-cd dist
-
 # place .nojekyll to bypass Jekyll processing
-echo > .nojekyll
+echo > dist/.nojekyll
 
-git config --global user.email "ci@example.com"
-git config --global user.name "CI"
+#git config --global user.email "ci@example.com"
+#git config --global user.name "CI"
 
-git init
-git checkout -B main
-git add -A
-git commit -m 'deploy'
-
-git push -f git@github.com:youssefbenlemlih/screen-color.git main:gh-pages
-
-cd -
+#git push -f git@github.com:youssefbenlemlih/screen-color.git main:gh-pages
+gh-pages -d dist
